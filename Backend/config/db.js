41d33a3ecd5826189de89a4df65ed44b5e890c78
sanskarkanade <kanadesanskar79@gmail.com>
2025-07-02 19:52:10 +0530

    mongobd cloude cluster added

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('✅ MongoDB connected');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
   } catch (err) {
-    console.error('❌ MongoDB connection error:', err.message);
-    process.exit(1);            // stop the app if DB fails
+    console.error("DB Connection Error:", err.message);
+    process.exit(1);
   }
 };
 
